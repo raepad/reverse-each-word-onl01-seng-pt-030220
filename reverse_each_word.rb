7dc1f0899289ffce59f1c75b95f5467c7f9ed36f
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-12 00:05:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
 
-#def reverse_each_word (hello_there)
-#  hello_there.split.collect {|word| word.reverse}.join(" ")
-#end
-
-def reverse_each_word(hello_there)
-  hello_there.collect.split do |hello|
-    hello.reverse.join(" ")
-  end
+def reverse_each_word (hello_there)
+  hello_there.split.collect {|word| word.reverse}.join(" ")
 end
